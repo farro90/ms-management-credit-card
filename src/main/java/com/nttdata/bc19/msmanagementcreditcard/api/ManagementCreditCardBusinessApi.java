f0e6f214967e3bddbda544d99b4f45c93ad90ca4
USERSAD\gfarrobe <gfarrobe@emeal.nttdata.com>
2022-06-22 17:18:18 -0500

    URL Api Gateway

--- src/main/java/com/nttdata/bc19/msmanagementcreditcard/api/ManagementCreditCardBusinessApi.java
+++ src/main/java/com/nttdata/bc19/msmanagementcreditcard/api/ManagementCreditCardBusinessApi.java
@@ -10,7 +10,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/current-account/business")
+@RequestMapping("/api/credit-card/business")
 public class ManagementCreditCardBusinessApi {
 
     @Autowired
